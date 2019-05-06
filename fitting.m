b5aea9a2e9cbb5567052a57c4df5ea031a2981a2
ivano696 <49634797+ivano696@users.noreply.github.com>
2019-05-06 11:20:37 +0200

    Add files via upload

--- fitting.m
+++ fitting.m
@@ -1,6 +1,19 @@
-%% Esempio 1-Fenomeno di Runge
-f = @(x) 1./(1 + 25*x.^2);
+%% Esempio 1-Interpolazione polinomiale di Lagrange
+x=1:6;
+y=rand(1,6);
 figure(1);
+nodi=plot(x,y,'o');
+hold on;
+t=linspace(1,6);
+p=polyfit(x,y,5);
+f=polyval(p,t);
+p1=plot(t,f,'r');
+title("Esempio di funzione interpolante");
+legend([nodi p1],{'Nodi','Interpolante Grado 5'});
+
+%% Esempio 2-Fenomeno di Runge
+f = @(x) 1./(1 + 25*x.^2);
+figure(2);
 x = linspace(-1,1,500);
 y_true = f(x);
 fun=plot(x,y_true,'r','linewidth',2);
@@ -33,9 +46,9 @@ plot(xdata,ydata,'k.','markersize',10);
 axis([-1 1 -0.5 1.5]);
 title("Fenomeno di Runge per polinomi interpolanti di vari gradi");
 legend([fun,poly_5, poly_15,poly_30],{'Funzione','N = 5','N = 15','N = 30'},'Location','north');
-%% Esempio 2-Fenomeno Runge Polinomio interpolante a tratti lineare
+%% Esempio 3-Fenomeno Runge Polinomio interpolante a tratti lineare
 f = @(x) 1./(1 + 25*x.^2);
-figure(1);
+figure(3);
 t = linspace(-1,1,500);
 y_true = f(t);
 fun=plot(t,y_true,'r','linewidth',2);
@@ -45,28 +58,34 @@ x = linspace(-1,1,5);
 y = f(x);
 pt=interp1(x,y,t);
 lin5=plot(t,pt,'k','linewidth',2);
-%N=10
-x1 = linspace(-1,1,10);
+%N=15
+x1 = linspace(-1,1,15);
 y1 = f(x1);
 pt1=interp1(x1,y1,t);
-lin10=plot(t,pt1,'m','linewidth',2);
-%N=100
-x2 = linspace(-1,1,100);
+lin15=plot(t,pt1,'m','linewidth',2);
+%N=30
+x2 = linspace(-1,1,30);
 y2 = f(x2);
 pt2=interp1(x2,y2,t);
-lin100=plot(t,pt2,'g','linewidth',2);
+lin30=plot(t,pt2,'g','linewidth',2);
 title("Fenomeno di Runge per interpolazioni lineare a tratti");
 axis([-1 1 -0.5 1.5]);
-legend([fun,lin5, lin10,lin100],{'Funzione','N = 5','N = 10','N = 100'},'Location','northeast');
-%% Esempio 3- Confronto tra polinomio interpolante e spline
-x=1:7;
-y=rand(1,7);
+legend([fun,lin5, lin15,lin30],{'Funzione','N = 5','N = 15','N = 30'},'Location','northeast');
+%Confronto con polinomio interpolante
 
-ppc=csape(x,y,'second');
-t=1:.1:7;
-ys=ppval(ppc,t);
-plot(x,y,'*',t,ys);
+%% Esempio 4- Confronto tra polinomio interpolante e spline naturale cubica
+x=1:10;
+y=rand(1,10);
+figure(1);
+punti=plot(x,y,'o');
+hold on;
 
-p=polyfit(x,y,5);
+pp=csape(x,y,'second');%Spline naturale cubica
+t=1:.1:10;
+ys=ppval(pp,t);
+spline=plot(t,ys,'g');
+
+p=polyfit(x,y,5); %Polinomio grado 5
 pl=polyval(p,t);
-plot(x,y,'o',t,ys,'g',t,pl,'y','LineWidth',2,'MarkerSize',6);
+plot(t,pl,'y');
+
